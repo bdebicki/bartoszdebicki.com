6d967c7c78242f4efbc9148be292c6e1059dada6
bdebicki <bartosz.debicki@zoin.pl>
2020-09-22 23:41:49 +0200

    polish link hover effect

--- src/js/enums/linkDecoratorStyles.js
+++ src/js/enums/linkDecoratorStyles.js
@@ -2,5 +2,3 @@ export const SOLID = 'solid';
 export const DASHED = 'dashed';
 export const DOTTED = 'dotted';
 export const DOUBLE = 'double';
-export const WAVY = 'wavy';
-export const ZIG_ZAG = 'zig-zag';
