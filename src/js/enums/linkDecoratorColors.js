1299f41ed0e46694d7b7cab4c6a227b9057ec507
bdebicki <bartosz.debicki@zoin.pl>
2020-09-29 16:19:20 +0200

    handle new palete for links and selection

--- src/js/enums/linkDecoratorColors.js
+++ src/js/enums/linkDecoratorColors.js
@@ -3,5 +3,3 @@ export const COLOR_2 = 'color2';
 export const COLOR_3 = 'color3';
 export const COLOR_4 = 'color4';
 export const COLOR_5 = 'color5';
-export const COLOR_6 = 'color6';
-export const COLOR_7 = 'color6';
