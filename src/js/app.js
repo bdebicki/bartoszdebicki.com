c08a4fbc6d58404cdb378c51ef88c282fa06d42b
bdebicki <bartosz.debicki@zoin.pl>
2020-09-29 23:00:36 +0200

    handle palette draw

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import setHoverDecorationEvents from './hoverDecorator/setHoverDecorationEvents';
 import setSelectionColorEvents from './selectionColor/setSelectionColorEvents';
+import setPalette from './paletteDraw/setPalette';
 
+window.addEventListener('load', setPalette);
 window.addEventListener('load', setHoverDecorationEvents);
 window.addEventListener('load', setSelectionColorEvents);
