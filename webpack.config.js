c16e42c9c147803e3b019e37feae5c63eb55c000
bdebicki <bartosz.debicki@zoin.pl>
2020-10-18 12:34:40 +0200

    add .editorconfig + fix indents in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,70 +5,70 @@ const PostCSSPresetEnv = require('postcss-preset-env');
 const SvgToMiniDataURI = require('mini-svg-data-uri');
 
 const config = {
-	devtool: 'eval',
-	entry: [
-		'./src/js/app.js',
-		'./src/less/app.less',
-	],
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bartoszdebicki.[hash].js',
-	},
-	resolve: {
-		modules: [__dirname, 'node_modules'],
-		extensions: ['*', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.less$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							plugins: () => [
-								PostCSSPresetEnv(),
-							],
-						},
-					},
-					'less-loader',
-				],
-			},
-			{
-				test: /\.svg$/i,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							generator: (content) => SvgToMiniDataURI(content.toString()),
-						},
-					},
-				],
-			},
-			{
-				test: /\.html$/,
-				loader: 'html-loader',
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/views/index.html',
-		}),
-		new MiniCssExtractPlugin({
-			filename: 'bartoszdebicki.[hash].css',
-		}),
-	],
+  devtool: 'eval',
+  entry: [
+    './src/js/app.js',
+    './src/less/app.less',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bartoszdebicki.[hash].js',
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                PostCSSPresetEnv(),
+              ],
+            },
+          },
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.svg$/i,
+        use: [
+  {
+            loader: 'url-loader',
+            options: {
+              generator: (content) => SvgToMiniDataURI(content.toString()),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/views/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bartoszdebicki.[hash].css',
+    }),
+  ],
 };
 
 exports.default = config;
